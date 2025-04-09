56fba3a23b4f52712ff0b7424b1e84f37e8c93d8
long.tran <long.tran@gogox.com>
2025-04-09 19:28:58 +0700

    Add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,15 @@ class MyHomePage extends StatelessWidget {
           notificationPredicate: (ScrollNotification notification) {
             return notification.depth == 1;
           },
+          scrolledUnderElevation: 4.0,
+          shadowColor: Theme.of(context).shadowColor,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: const Icon(Icons.cloud_outlined), text: titles[0],),
+              Tab(icon: const Icon(Icons.beach_access_sharp), text: titles[1],),
+              Tab(icon: const Icon(Icons.brightness_5_sharp), text: titles[2],),
+            ],
+          ),
         ),
       ),
     );
